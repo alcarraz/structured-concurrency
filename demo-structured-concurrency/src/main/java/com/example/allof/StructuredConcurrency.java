da8998d8c5575948da47d6f6acfca432b6f570d1
Andrés Alcarraz <alcarraz@gmail.com>
2024-10-03 19:03:25 -0500

    Print finishing in finally block

--- demo-structured-concurrency/src/main/java/com/example/allof/StructuredConcurrency.java
+++ demo-structured-concurrency/src/main/java/com/example/allof/StructuredConcurrency.java
@@ -12,15 +12,17 @@ import static com.example.Validations.checkPIN;
 
 public class StructuredConcurrency {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        Transaction tx = new Transaction("1234567890123456", "1234", -100.0, "123456");
+        Transaction tx = new Transaction("1234567890123456", "1234", 100.0, "123456");
         try (StructuredTaskScope.ShutdownOnFailure scope = new StructuredTaskScope.ShutdownOnFailure()) {
             Subtask<Boolean> checkBalance = scope.fork(() -> checkBalance(tx.cardNumber(), tx.amount()));
             Subtask<Boolean> checkMerchant = scope.fork(() -> checkMerchant(tx.merchantId()));
             Subtask<Boolean> checkPIN = scope.fork(() -> checkPIN(tx.cardNumber(), tx.pinBlock()));
-            scope.join().throwIfFailed();
+            scope.join()
+                    .throwIfFailed(); // If this is missing, an IllegalStateException is thrown if a subtask fails
             boolean success = checkBalance.get() && checkMerchant.get() && checkPIN.get();
             System.out.println("All validations completed: " + success );
+        } finally {
+            System.out.println("Finishing");
         }
-        System.out.println("Finishing");
     }
 }
