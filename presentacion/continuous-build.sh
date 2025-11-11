f9a34c3727c138c6e9b45298f380576aa7d14d4a
Andrés Alcarraz <alcarraz@gmail.com>
2025-11-11 07:58:27 -0500

    Use subsections for the frame subtitles.
    Show subsection to the right

--- presentacion/continuous-build.sh
+++ presentacion/continuous-build.sh
@@ -5,8 +5,10 @@ make quick
 echo "Watching for .tex file changes..."
 echo "Press Ctrl+C to stop"
 
-inotifywait -m -r -e modify,create,move --include '.*\.tex$' --format '%w%f' . | while read file
+inotifywait -m -r -e modify,create,move --include '.*\.(tex|cls)$' --format '%w%f' . | while read file
 do
     echo "$(date '+%Y-%m-%d %H:%M:%S'): Detected change in $file"
     make quick
+    echo "$(date '+%Y-%m-%d %H:%M:%S'): presentation compiled"
+    
 done
